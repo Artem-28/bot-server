2773283e07d68869da04e53fa2ec9ee1f1396d3a
Artem Mikheev <artem.mikheev.git@gmail.com>
2023-08-30 17:48:02 +0300

    create config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true, // Включение\отключение глобальной обрасти для конфига .env
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
